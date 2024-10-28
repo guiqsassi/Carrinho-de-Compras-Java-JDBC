233be4b824e0a92e594e94e2af5eeac2dd04fcc2
guiqsassi <guiqsassi@gmail.com>
2024-10-28 18:10:44 -0300

    feat: conexao com o banco de dados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import db.Db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
     }
 }
