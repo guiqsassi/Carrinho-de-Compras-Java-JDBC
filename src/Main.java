e5ee70b3ad6beb3b8f721c9d29dde17c37d04217
guiqsassi <guiqsassi@gmail.com>
2024-10-29 13:57:13 -0300

    refactor: remove imports desncessários na Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-import db.Db;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+
     }
 }
