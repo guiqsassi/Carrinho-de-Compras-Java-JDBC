b169c626031e8775a0f5de2567af2405cceecc69
guiqsassi <guiqsassi@gmail.com>
2024-10-29 16:53:51 -0300

    feat:cria a classe Stock

---
+++ src/model/entities/Stock.java
@@ -0,0 +1,63 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Stock implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    private String category;
+    private Double valor;
+    private Integer quantity;
+
+    public Stock() {}
+
+    public Stock(Integer id, String name, String category, Double valor, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.valor = valor;
+        this.quantity = quantity;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
