0dba2df322c8b602b4461c2f3c9f67bd08326156
guiqsassi <guiqsassi@gmail.com>
2024-10-30 17:34:01 -0300

    refactor: metodo consultar carrinho por id, agora tambem retorna os itens inseridos neste carrinho

--- src/model/dao/impl/CartDaoJDBC.java
+++ src/model/dao/impl/CartDaoJDBC.java
@@ -3,6 +3,8 @@ package model.dao.impl;
 import db.Db;
 import model.dao.CartDao;
 import model.entities.Cart;
+import model.entities.CartItem;
+import model.entities.Stock;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,6 +22,7 @@ public class CartDaoJDBC implements CartDao {
 
     @Override
     public void insert(Cart cart) {
+
     }
 
     @Override
@@ -38,15 +41,28 @@ public class CartDaoJDBC implements CartDao {
         ResultSet rs = null;
 
         try{
-            ps = conn.prepareStatement("SELECT * FROM cart WHERE id = ?");
-            ps.setInt(1, id);
+            ps = conn.prepareStatement("SELECT cart.id, cart.quantity, cart.total_value ,cart_item.quantity as \"item_quantity\", stock.name, stock.value, stock.quantity AS \"stQuantity\"," +
+                    " stock.id AS \"stId\" FROM cart inner join cart_item \n" +
+                    "ON cart.id = cart_item.cartId\n" +
+                    "INNER JOIN stock\n" +
+                    "ON cart_item.stockId = stock.id" +
+                    " WHERE cart.id = ?");
 
+            ps.setInt(1, id);
             rs = ps.executeQuery();
 
             Cart cart = new Cart();
+
             if(rs.next()) {
                 cart = cartInstantiation(rs);
             }
+
+            do{
+                CartItem cartItem = cartItemInstantiation(rs);
+                cart.addItem(cartItem);
+            }
+            while (rs.next());
+
             return cart;
 
         } catch (SQLException e) {
@@ -76,5 +92,23 @@ public class CartDaoJDBC implements CartDao {
         }
 
     }
+    public CartItem cartItemInstantiation(ResultSet rs){
+        CartItem cartItem = new CartItem();
+        Stock stock = new Stock();
+        try {
+            cartItem.setId(rs.getInt("id"));
+            cartItem.setQuantity(rs.getInt("item_quantity"));
+            stock.setId(rs.getInt("stId"));
+            stock.setQuantity(rs.getInt("stQuantity"));
+            stock.setPrice(rs.getDouble("value"));
+            stock.setName(rs.getString("name"));
+            cartItem.setStock(stock);
+
+            return cartItem;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
